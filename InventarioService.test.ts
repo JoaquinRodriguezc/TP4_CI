caa1d49803b6ddc1a6d33e24460dc319c6806ce8
Matias Roiz <roizmatias10@gmail.com>
2025-06-23 09:34:33 -0300

    test mati roiz

--- InventarioService.test.ts
+++ InventarioService.test.ts
@@ -3,18 +3,57 @@ import { InventarioService } from "./InventarioService";
 
 describe("InventarioService", () => {
   const inventarioService = new InventarioService();
+  //-------------------------------------------------------------------------------------
   it("should be defined", () => {
     expect(inventarioService).toBeDefined();
   });
-
+  //-------------------------------------------------------------------------------------
   it("debe lanzar error porque un valor es negativo", () => {
     expect(() => inventarioService.calcularLoteOptimo(10, 10, -10)).toThrow();
   });
+  //-------------------------------------------------------------------------------------
   it("debe lanzar error por costoMantenimiento no puede ser 0", () => {
     expect(() => inventarioService.calcularLoteOptimo(10, 10, 0)).toThrow();
   });
+  //-------------------------------------------------------------------------------------
   it("debe ser el valor correcto", () => {
     const qOPtimo = inventarioService.calcularLoteOptimo(1000, 10, 30);
     expect(qOPtimo).toBeCloseTo(25.819);
   });
+  //-------------------------------------------------------------------------------------
+  it("debe calcular correctamente el Costo Total de Inventario (caso simple)", async () => {
+    const result = await inventarioService.calcularCostoTotalLoteFijo(
+      100,   // loteOptimo
+      1000,  // demanda
+      10,    // costoUnidad
+      50,    // costoPedido
+      2      // costoMantenimiento
+    );
+    expect(result).toBeCloseTo(10600);
+  });
+  //-------------------------------------------------------------------------------------
+  it("Debe lanzar error si el lote óptimo es 0", async () => {
+    await expect(
+      inventarioService.calcularCostoTotalLoteFijo(
+        0,
+        1000,
+        10,
+        50,
+        2
+      )
+    ).rejects.toThrow("No se puede calcular CT con lote óptimo igual a 0 o nulo");
+  });
+  //-------------------------------------------------------------------------------------
+  it("debe calcular correctamente un caso realista y complejo (≈19690.83)", async () => {
+    const result = await inventarioService.calcularCostoTotalLoteFijo(
+      150,     // loteOptimo
+      1450,    // demanda
+      12.75,   // costoUnidad
+      95,      // costoPedido
+      3.8      // costoMantenimiento
+    );
+    expect(result).toBeCloseTo(19690.83, 2); // Usá 2 decimales para que sea más estricto
+  });
+  //-------------------------------------------------------------------------------------
 });
+
